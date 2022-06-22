81b0a97234c5bced93e72535570f82d78d71cdc4
Steve Vezeau <svezer@live.ca>
2022-06-21 23:06:09 -0700

    format for line length

--- dpk.js
+++ dpk.js
@@ -8,24 +8,28 @@ const crypto = require("crypto");
  * @param  {...(T) => K} fns A parameter list of functions.
  * @returns A new function which accepts a parameter to be passed into the pipe.
  */
-const pipe = (...fns) => start => fns.reduce((param, cur) => cur(param), start);
+const pipe =
+  (...fns) =>
+  (start) =>
+    fns.reduce((param, cur) => cur(param), start);
 
 /**
  * Internalization of config for our hash implementation.
  * @param {crypto.BinaryLike} value Input data.
  * @returns {string} The digest string.
  */
-const hash = value => crypto.createHash("sha3-512").update(value).digest("hex");
+const hash = (value) =>
+  crypto.createHash("sha3-512").update(value).digest("hex");
 
 /**
  * Closes over a hash implementation and decides how to treat the input.
- * 
+ *
  * Try to use the event.partitionKey, when it exists.
  * Otherwise fallback to a string representation of the event.
  * @param {typeof hash} hash A hash function.
  * @returns {any} A candidate.
  */
-const extractInput = hash => event => {
+const extractInput = (hash) => (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   if (!event) return TRIVIAL_PARTITION_KEY;
 
@@ -39,14 +43,15 @@ const extractInput = hash => event => {
  * @param {*} candidate A potential partition key.
  * @returns {string} A string version of the candidate.
  */
-const ensureCandidateType = candidate => typeof candidate === "string" ? candidate : JSON.stringify(candidate);
+const ensureCandidateType = (candidate) =>
+  typeof candidate === "string" ? candidate : JSON.stringify(candidate);
 
 /**
  * Generates a new hash when the candidate length is too long.
  * @param {typeof hash} hash A hash function.
- * @returns 
+ * @returns
  */
-const ensureMaxLength = hash => candidate => {
+const ensureMaxLength = (hash) => (candidate) => {
   const MAX_PARTITION_KEY_LENGTH = 256;
   if (candidate.length > MAX_PARTITION_KEY_LENGTH) return hash(candidate);
 
@@ -55,18 +60,18 @@ const ensureMaxLength = hash => candidate => {
 
 /**
  * Generate a partition key.
- * 
+ *
  * The same key will be generated given the same input.
- * 
+ *
  * Strictly speaking, determinism is based on:
  * 1. the start.partitionKey being truthy and being the same, or
  * 2. the string representation of start being the same, or
  * 3. start being falsey.
  * @param {{ partitionKey?: any }} start
- * @returns {string} 
+ * @returns {string}
  */
 exports.deterministicPartitionKey = pipe(
   extractInput(hash),
   ensureCandidateType,
-  ensureMaxLength(hash),
+  ensureMaxLength(hash)
 );
